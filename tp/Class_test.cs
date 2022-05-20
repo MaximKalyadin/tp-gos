3f00c9039b04bc2b195f39ab43cdf3083dc1d371
Maxim_Kalyadin <kalyadin.maxim@gmail.com>
2022-05-20 13:48:05 +0400

    wf

---
+++ tp/Class_test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace tp
+{
+    [XmlRoot]
+    public class Class_test
+    {
+        [XmlElement]
+        public int id { get; set; }
+        [XmlAttribute(AttributeName = "Number")]
+        public int number { get; set; }
+        [XmlAttribute(AttributeName = "Title")]
+        public string title { set; get; }
+        [XmlAttribute(AttributeName = "Look")]
+        public bool is_look { set; get; }
+
+        public Class_test() { }
+    }
+}
---
+++ tp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace tp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Class_test()
+            {
+                id = 1,
+                is_look = true,
+                number = 7887,
+                title = "Название"
+            };
+            var f = new XmlSerializer(typeof(Class_test));
+            using(var fs = new FileStream($"{test.title}.xml", FileMode.OpenOrCreate))
+            {
+                f.Serialize(fs, test);
+            }
+
+            using (var fs = new FileStream($"{test.title}.xml", FileMode.OpenOrCreate))
+            {
+                var test_des = (Class_test)f.Deserialize(fs);
+                Console.WriteLine(test);
+                Console.WriteLine(test_des);
+            }
+            
+            // var docLoad = new System.Xml.Linq.XDocument.Load("Названиею.xml");
+            List<Class_test> list_test = new List<Class_test>();
+            for (int i = 0; i < 100; i++)
+            {
+                list_test.Add(new Class_test()
+                {
+                    id = i,
+                    is_look = (i % 2 == 0),
+                    number = i + 23,
+                    title = $"Название {i}"
+                });
+            }
+
+            var st = list_test
+                .Where(x => x.is_look && x.id > 20)
+                .OrderByDescending(x => x.number)
+                .Select(x => x.title);
+
+            Console.WriteLine(st);
+
+        }
+    }
+}
